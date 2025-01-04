a303e0254320023a57f2db8054e55a3264f727d2
Susanta Gorai <goraisusanta2018@gmail.com>
2025-01-04 23:46:53 +0530

    Some quize solve

---
+++ QuizeTest.java
@@ -0,0 +1,32 @@
+class QuizeTest {
+
+    public static void main(String[] args) {
+        // char ch='a';
+        // char res=(char)(ch+1);
+        // System.out.println(res);
+
+        // byte b1 = 5, b2 = 6;
+
+        // byte b3 = (byte) (b1 + b2);
+
+        // System.out.println(b3);
+
+        // int a = 3;
+        // int b = 6;
+        // int result = (~a & b) | (a & ~b);
+        // System.out.println(result);
+
+        int i, j;
+
+        i = 100;
+
+        j = 300;
+
+        System.out.println(++i + " " + --j);
+
+        while (++i < --j) {
+            System.out.println(i);
+        }
+
+    }
+}
